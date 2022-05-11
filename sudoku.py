7318c0cadd89a1249c0f9174889bb557d58866ee
Aiden Marchiori <36175783+Element21@users.noreply.github.com>
2022-05-11 11:59:43 -0400

    Update graphics to decrease load on terminal clearing, increasing speed

--- sudoku.py
+++ sudoku.py
@@ -1,21 +1,20 @@
 import random
-import time
-import os
 
 # Fill in the board here. (0 = value to find)
 
-sudoku_board = [ # World's hardest sudoku (takes time on repl! PLEASE RUN LOCALLY)
-    [8, 0, 0,   0, 0, 0,    0, 0, 0],
-    [0, 0, 3,   6, 0, 0,    0, 0, 0],
-    [0, 7, 0,   0, 9, 0,    2, 0, 0],
+sudoku_board = [  # World's hardest sudoku (takes time on repl! PLEASE RUN LOCALLY)
+    [8, 0, 0,  0, 0, 0,  0, 0, 0],
+    [0, 0, 3,  6, 0, 0,  0, 0, 0],
+    [0, 7, 0,  0, 9, 0,  2, 0, 0],
 
-    [0, 5, 0,   0, 0, 7,    0, 0, 0],
-    [0, 0, 0,   0, 4, 5,    7, 0, 0],
-    [0, 0, 0,   1, 0, 0,    0, 3, 0],
+    [0, 5, 0,  0, 0, 7,  0, 0, 0],
+    [0, 0, 0,  0, 4, 5,  7, 0, 0],
+    [0, 0, 0,  1, 0, 0,  0, 3, 0],
 
-    [0, 0, 1,   0, 0, 0,    0, 6, 8],
-    [0, 0, 8,   5, 0, 0,    0, 1, 0],
-    [0, 9, 0,   0, 0, 0,    4, 0, 0]]
+    [0, 0, 1,  0, 0, 0,  0, 6, 8],
+    [0, 0, 8,  5, 0, 0,  0, 1, 0],
+    [0, 9, 0,  0, 0, 0,  4, 0, 0],
+]
 
 # 8  1  2  |  7  5  3  |  6  4  9 SOLUTION
 # 9  4  3  |  6  8  2  |  1  7  5
@@ -29,43 +28,35 @@ sudoku_board = [ # World's hardest sudoku (takes time on repl! PLEASE RUN LOCALL
 # 4  3  8  |  5  2  6  |  9  1  7
 # 7  9  6  |  3  1  8  |  4  5  2
 
-
-N = 9  # Length and width of board
-size = N * N
-
+SIDE_LEN = 9  # Length and width of board
+board_size = SIDE_LEN * SIDE_LEN
 
 def get_row(board, row_num):
-    return board[row_num][0:N]
-
+    return board[row_num][0:SIDE_LEN]
 
 def get_col(board, col_num):
-    return [board[i][col_num] for i in range(N)]
-
+    return [board[i][col_num] for i in range(SIDE_LEN)]
 
 def get_block(board, block_num):
     row = (block_num // 3) * 3
-    col = (block_num * 3) % N
-    return [board[row + i // 3][col + i % 3] for i in range(N)]
-
+    col = (block_num * 3) % SIDE_LEN
+    return [board[row + i // 3][col + i % 3] for i in range(SIDE_LEN)]
 
 def print_board(board):
-  # Used for those EPIC terminal graphics
-    time.sleep(0.06)
-    os.system('clear')
-    for row in range(N):
+    # Used for those EPIC terminal graphics
+    print("\033[2;0H", end="")
+    for row in range(SIDE_LEN):
         if row in (3, 6):
-            print('-------------------------------')
+            print("-------------------------------")
 
-        for col in range(N):
+        for col in range(SIDE_LEN):
             if col in (3, 6):
-                print('|  ', end='')
-            print(board[row][col], ' ', end='')
+                print("|  ", end="")
+            print(board[row][col], " ", end="")
         print()
-
 
 def row_col_to_block(row, col):
     return col // 3 + 3 * (row // 3)
-
 
 def is_possible(board, row_num, col_num, input_num):
     row = get_row(board, row_num)
@@ -82,13 +73,12 @@ def is_possible(board, row_num, col_num, input_num):
 
     return True
 
-
 def is_solved(board):
 
     # check if valid
     complete = set(range(1, 10))
 
-    for i in range(N):
+    for i in range(SIDE_LEN):
         row = get_row(board, i)
         col = get_col(board, i)
         block = get_block(board, i)
@@ -102,29 +92,25 @@ def is_solved(board):
 
     return True
 
-
 def next_loc(board):
-    for row in range(N):
-        for col in range(N):
+    for row in range(SIDE_LEN):
+        for col in range(SIDE_LEN):
             if board[row][col] == 0:
                 return row, col
     return None, None
 
-
 def solve(board, turn_num=-1):
-    print('=' * 43)
+    print("\033[2;0H", end="")
     print_board(board)
     count = 0
-    for row in range(N):
-        for col in range(N):
+    for row in range(SIDE_LEN):
+        for col in range(SIDE_LEN):
             if board[row][col] == 0:
                 count += 1
     print()
-    print('=' * 43)
-    print("Recursion Depth {} -- Percent Solved {:3.1f}%".format(
-        turn_num,
-        100 * (size - count) / size
-    ))
+    print(
+        f"Recursion Depth: {turn_num} -- Percent Solved {100 * (board_size - count) / board_size:3.1f}%"
+    )
 
     if is_solved(board):
         print_board(board)
@@ -133,7 +119,6 @@ def solve(board, turn_num=-1):
     # recurse
     row, col = next_loc(board)
 
-
     for val in range(1, 10):
         if is_possible(board, row, col, val):
             board[row][col] = val
@@ -141,19 +126,18 @@ def solve(board, turn_num=-1):
                 # the board is solved.
                 return True
             # undo move, couldn't solve
-            board[row][col] = 0
+            else:
+                board[row][col] = 0
 
     return False
-
 
 # ========================= MAIN =========================
 try:
     if solve(sudoku_board):
-        print('\nSolution Found!')
+        print("\n\nSolution Found!")
     else:
-        print("\nIMPOSSIBLE.")
+        print("\n\nIMPOSSIBLE.")
 
 except KeyboardInterrupt:
-    os.system('clear')
     print_board(sudoku_board)
-    print('\nSolving Canceled...')
+    print("\nSolving Canceled...")
